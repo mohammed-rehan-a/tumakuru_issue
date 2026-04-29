784268bdbfbd4f0a583d27cdd6c6435e0db874b0
Rehan-commits <tufgamer1890@gmail.com>
2026-04-29 20:27:40 +0530

    Auto-create superuser on deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata categories_fixture.json
+
+# Auto-create superuser on deployment (because Render Free tier has no shell)
+DJANGO_SUPERUSER_PASSWORD=AdminPass123! \
+DJANGO_SUPERUSER_USERNAME=admin_render \
+DJANGO_SUPERUSER_EMAIL=admin@example.com \
+python manage.py createsuperuser --noinput || true
